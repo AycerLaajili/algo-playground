bdb76cde2dc3be97e7232cea0cab830721d98e9c
Aycer Lajili <Laajiliaycer6@gmail.com>
2021-03-10 13:56:46 +0100

    isSorted

--- main.js
+++ main.js
@@ -42,3 +42,19 @@ function minmaxElement() {
 
     console.log(`Output: Min = ${minValue}, Max = ${maxValue}`)
 }
+
+function isSorted(){
+    let tab = [5, 8, 9, 20, 21, 44]
+    console.log('Input: ',tab)
+
+    let sorted =true
+    for (let i = 1; i < tab.length; i++) {
+        if (tab[i] <tab [i-1]) {
+            sorted=false
+            break
+        }
+       
+    }
+
+    console.log (sorted)
+}
