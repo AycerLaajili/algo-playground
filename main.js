2e905be6a50bc2cecf7cc8b281a15165d3b58edb
Aycer Lajili <Laajiliaycer6@gmail.com>
2021-03-10 12:37:41 +0100

    minElement

--- main.js
+++ main.js
@@ -6,7 +6,20 @@ function maxElement() {
     for (let i = 0; i < array.length; i++) {
         if (array[i] > MaxValue)
             MaxValue = array[i]
-
     }
+
     console.log('Output:', MaxValue)
+}
+
+function minElement() {
+    let array = [5, 8, 2, 3, 9, 4]
+    console.log('Input: ', array)
+
+    let MinValue = array[0]
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < MinValue)
+            MinValue = array[i]
+    }
+
+    console.log('Output:', MinValue)
 }
